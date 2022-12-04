9b103e43a2c624ba12e3bd47f6317bdb669c4e7a
Mathias Wöß <mat.woess@gmail.com>
2022-12-04 17:13:56 +0100

    add day4 solution in python

---
+++ day04.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from dataclasses import dataclass
+
+
+@dataclass
+class Assignment:
+    sections1: set
+    sections2: set
+
+    @classmethod
+    def from_string(cls, string: str) -> 'Assignment':
+        assignment1, assignment2 = string.split(",")
+        sections1 = cls.get_section_range(assignment1)
+        sections2 = cls.get_section_range(assignment2)
+        return Assignment(set(sections1), set(sections2))
+
+    @staticmethod
+    def get_section_range(assignment: str) -> range:
+        section_from, section_to = map(int, assignment.split('-'))
+        return range(section_from, section_to + 1)
+
+
+def part1(assignments: list[Assignment]) -> int:
+    count = 0
+    for a in assignments:
+        if a.sections1.issubset(a.sections2) or a.sections2.issubset(a.sections1):
+            count += 1
+    return count
+
+
+def part2(assignments: list[Assignment]) -> int:
+    count = 0
+    for a in assignments:
+        if (a.sections1 & a.sections2) != set():
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    raw_data = Path('inputs/day04.txt').read_text(encoding='utf-8')
+    data = [Assignment.from_string(line) for line in raw_data.splitlines()]
+    answer1 = part1(data)
+    print('Answer1:', answer1)  # 588
+    answer2 = part2(data)
+    print('Answer2:', answer2)  # 911
